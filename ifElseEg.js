2958db8eacbab662574a0984fc9c55a6da6a3f73
JayadharshiniK <kvjayadharshini@gmail.com>
2023-07-19 17:05:30 +0530

    js

---
+++ ifElseEg.js
@@ -0,0 +1,8 @@
+
+var age = 25;
+
+if (age > 30) {
+    console.log("senior");
+} else {
+    console.log("Junior")
+}
---
+++ variablesEg.js
@@ -0,0 +1,6 @@
+var firstName = "Allan";
+let lastName = "Brown";
+let age = 25;
+
+console.log(firstName );
+console.log("Age : " + age);
